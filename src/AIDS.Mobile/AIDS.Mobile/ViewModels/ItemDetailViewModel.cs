873e34463cd09479770d49a8104747d92581dae4
Jarred Capellman <jcapellman@hotmail.com>
2020-02-13 20:04:48 -0600

    Added: iOS and Android App

---
+++ src/AIDS.Mobile/AIDS.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AIDS.Mobile.Models;
+
+namespace AIDS.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}

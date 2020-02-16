0b3a86960ad03a3d38d0539e383f456afae84da6
Jarred Capellman <jcapellman@hotmail.com>
2020-02-16 11:36:55 -0600

    Change: Updated the ItemsPage to support the new Properties

--- src/AIDS.Mobile/AIDS.Mobile/Views/ItemsPage.xaml.cs
+++ src/AIDS.Mobile/AIDS.Mobile/Views/ItemsPage.xaml.cs
@@ -13,8 +13,6 @@ using AIDS.Mobile.ViewModels;
 
 namespace AIDS.Mobile.Views
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class ItemsPage : ContentPage
     {
@@ -27,29 +25,17 @@ namespace AIDS.Mobile.Views
             BindingContext = viewModel = new ItemsViewModel();
         }
 
-        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
-        {
-            var item = args.SelectedItem as Item;
-            if (item == null)
-                return;
-
-            await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
-
-            // Manually deselect item.
-            ItemsListView.SelectedItem = null;
-        }
-
-        async void AddItem_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
-        }
-
         protected override void OnAppearing()
         {
             base.OnAppearing();
 
             if (viewModel.Items.Count == 0)
                 viewModel.LoadItemsCommand.Execute(null);
+        }
+
+        private void Sync_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }

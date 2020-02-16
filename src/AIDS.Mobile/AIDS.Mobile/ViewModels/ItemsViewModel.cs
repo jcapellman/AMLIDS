098ffa4f3e08f9714ccd781662536c25d42f0d8f
Jarred Capellman <jcapellman@hotmail.com>
2020-02-16 11:35:56 -0600

    Change: Extensive work on the ItemsViewModel

--- src/AIDS.Mobile/AIDS.Mobile/ViewModels/ItemsViewModel.cs
+++ src/AIDS.Mobile/AIDS.Mobile/ViewModels/ItemsViewModel.cs
@@ -1,45 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 
 using AIDS.Mobile.Models;
-using AIDS.Mobile.Views;
+using AIDS.Mobile.Extensions;
 
 namespace AIDS.Mobile.ViewModels
 {
     public class ItemsViewModel : BaseViewModel
     {
         public ObservableCollection<Item> Items { get; set; }
+
         public Command LoadItemsCommand { get; set; }
 
         public ItemsViewModel()
         {
-            Title = "Browse";
+            Title = "Traffic";
             Items = new ObservableCollection<Item>();
-            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
-
-            MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
-            {
-                var newItem = item as Item;
-                Items.Add(newItem);
-                await DataStore.AddItemAsync(newItem);
-            });
+            LoadItemsCommand = new Command(() => ExecuteLoadItemsCommand());
         }
 
-        async Task ExecuteLoadItemsCommand()
+        private List<Item> GetConnections()
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/system/bin/netstat",
+                    Arguments = "-n",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+
+            var items = new List<Item>();
+
+            while (!process.StandardOutput.EndOfStream)
+            {
+                var item = process.StandardOutput.ReadLine().ToItem();
+
+                if (item == null)
+                {
+                    continue;
+                }
+
+                items.Add(item);
+            }
+
+            process.WaitForExit();
+
+            return items;
+        }
+
+        void ExecuteLoadItemsCommand()
         {
             if (IsBusy)
+            {
                 return;
+            }
 
             IsBusy = true;
 
             try
             {
-                Items.Clear();
-                var items = await DataStore.GetItemsAsync(true);
+                var items = GetConnections();
+
                 foreach (var item in items)
                 {
                     Items.Add(item);

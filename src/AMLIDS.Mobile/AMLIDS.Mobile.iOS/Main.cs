2c29a04aa5cf71e9e4aab795aa5d79cf094e5ca7
Jarred Capellman <jcapellman@hotmail.com>
2020-02-16 22:12:20 -0600

    Change: Updated the iOS app to match the new App Name

--- src/AMLIDS.Mobile/AMLIDS.Mobile.iOS/Main.cs
+++ src/AMLIDS.Mobile/AMLIDS.Mobile.iOS/Main.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using Foundation;
 using UIKit;
 
-namespace AIDS.Mobile.iOS
+namespace AMLIDS.Mobile.iOS
 {
     public class Application
     {
--- src/AMLIDS.Mobile/AMLIDS.Mobile.iOS/Properties/AssemblyInfo.cs
+++ src/AMLIDS.Mobile/AMLIDS.Mobile.iOS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AIDS.Mobile.iOS")]
+[assembly: AssemblyTitle("AMLIDS.Mobile.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AIDS.Mobile.iOS")]
+[assembly: AssemblyProduct("AMLIDS.Mobile.iOS")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

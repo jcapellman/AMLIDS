800f2117ce7d22e8759ee1677a1c975a47ca46b8
Jarred Capellman <jcapellman@hotmail.com>
2020-02-17 09:19:07 -0600

    Added: Population of the properties given an object in the NetworkDataItem #2

--- src/AMLIDS.lib.dal/Models/NetworkDataItem.cs
+++ src/AMLIDS.lib.dal/Models/NetworkDataItem.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+
+using System;
 
 namespace AMLIDS.lib.dal.Models
 {
     public class NetworkDataItem
     {
+        public int Id { get; set; }
+
         public int Version { get; set; }
 
         public DateTimeOffset Timestamp { get; set; }
 
         public string JSON { get; set; }
+
+        public NetworkDataItem() { }
+
+        public NetworkDataItem(object payload)
+        {
+            Version = Common.Constants.DATA_DEFINITION_VERSION;
+
+            JSON = JsonConvert.SerializeObject(payload);
+
+            Timestamp = DateTimeOffset.Now;
+        }
     }
 }

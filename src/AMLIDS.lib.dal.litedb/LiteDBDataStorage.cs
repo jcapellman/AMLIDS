548ba07ef2743f154612edefae6e6b2e1c59377c
Jarred Capellman <jcapellman@hotmail.com>
2020-02-17 09:18:00 -0600

    Added: LiteDBDataStorage implementation #3

---
+++ src/AMLIDS.lib.dal.litedb/LiteDBDataStorage.cs
@@ -0,0 +1,49 @@
+﻿using AMLIDS.lib.dal.Enums;
+using AMLIDS.lib.dal.Models;
+
+using LiteDB;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace AMLIDS.lib.dal.litedb
+{
+    public class LiteDBDataStorage : IDataStorage
+    {
+        private const string DB_FILENAME = "ds.db";
+
+        public List<NetworkDataItem> GetAllNetworkData()
+        {
+            using (var db = new LiteDatabase(DB_FILENAME))
+            {
+                var collection = db.GetCollection<NetworkDataItem>();
+
+                return collection.FindAll().ToList();
+            }
+        }
+
+        public OperationResponse InsertNetworkData(object payload)
+        {
+            using (var db = new LiteDatabase(DB_FILENAME))
+            {
+                var collection = db.GetCollection<NetworkDataItem>();
+
+                collection.Insert(new NetworkDataItem(payload));
+
+                return OperationResponse.SUCCESS;
+            }
+        }
+
+        public List<NetworkDataItem> QueryNetworkData(Expression<Func<NetworkDataItem, bool>> query)
+        {
+            using (var db = new LiteDatabase(DB_FILENAME))
+            {
+                var collection = db.GetCollection<NetworkDataItem>();
+
+                return collection.Find(query).ToList();
+            }
+        }
+    }
+}

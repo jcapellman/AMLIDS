4fadd2015422e81c5362497817ce6f66d68ad783
Jarred Capellman <jcapellman@hotmail.com>
2020-02-17 20:03:55 -0600

    Change: Swapped out the hard coded value with a reference to the IVersionService #20

--- src/AMLIDS.Mobile/AMLIDS.Mobile/ViewModels/AboutViewModel.cs
+++ src/AMLIDS.Mobile/AMLIDS.Mobile/ViewModels/AboutViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows.Input;
 
+using AMLIDS.Mobile.Services;
+
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -7,10 +9,26 @@ namespace AMLIDS.Mobile.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        private string _versionString;
+
+        public string VersionString
+        {
+            get => _versionString;
+
+            set
+            {
+                _versionString = value;
+
+                OnPropertyChanged();
+            }
+        }
+
         public AboutViewModel()
         {
             Title = "About";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://github.com/jcapellman/AMLIDS/"));
+
+            VersionString = $"Version {DependencyService.Get<IVersionService>().VersionNumber} (BUILD {DependencyService.Get<IVersionService>().BuildNumber})";
         }
 
         public ICommand OpenWebCommand { get; }

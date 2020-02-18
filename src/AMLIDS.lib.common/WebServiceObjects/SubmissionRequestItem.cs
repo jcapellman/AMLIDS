1cb3e40ecf494f75d0428761087f304bec55a830
Jarred Capellman <jcapellman@hotmail.com>
2020-02-17 20:27:42 -0600

    Added: SubmissionRequestItem class to the common lib

---
+++ src/AMLIDS.lib.common/WebServiceObjects/SubmissionRequestItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using AMLIDS.lib.common.Models;
+
+namespace AMLIDS.lib.common.WebServiceObjects
+{
+    public class SubmissionRequestItem
+    {
+        public DeviceInformationItem DeviceInformation { get; set; }
+
+        public List<RawNetworkCaptureItem> RawNetworkCaptureItems { get; set; }
+    }
+}

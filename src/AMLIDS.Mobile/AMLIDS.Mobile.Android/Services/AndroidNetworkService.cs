140b085ff90c33f23b3508f6f801a49c97f851a0
Jarred Capellman <jcapellman@hotmail.com>
2020-02-18 19:45:26 -0600

    Added: Initial work on the NetworkStatsManager

--- src/AMLIDS.Mobile/AMLIDS.Mobile.Android/Services/AndroidNetworkService.cs
+++ src/AMLIDS.Mobile/AMLIDS.Mobile.Android/Services/AndroidNetworkService.cs
@@ -1,15 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 using AMLIDS.lib.common.Models;
 using AMLIDS.Mobile.Services;
+
+using Android.App.Usage;
+using Android.Content;
+using Android.Net;
 
 namespace AMLIDS.Mobile.Droid.Services
 {
     public class AndroidNetworkService : INetworkService
     {
+        private readonly NetworkStatsManager _networkStatsManager;
+
+        public AndroidNetworkService()
+        {
+            _networkStatsManager = (NetworkStatsManager)Android.App.Application.Context.GetSystemService(Context.NetworkStatsService);
+        }
+
         public List<RawNetworkCaptureItem> GetNetworkData()
         {
-            
+            var bucket = _networkStatsManager.QuerySummaryForDevice(ConnectivityType.Mobile, "", 0, 1000);
+
+            return new List<RawNetworkCaptureItem>();
         }
     }
 }
